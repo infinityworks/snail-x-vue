7ae5a81aa4a029c4fa019612e93130d398ef8712
Emily <emily.birch@infinityworks.com>
2018-10-25 17:15:06 +0100

    fixed display predictions bug

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,168 +1,239 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
-import axios from 'axios'
+<template>
+        <div id="home" class="home-body">
+        <!--<h1 v-if="!loggedIn" style="color: whitesmoke">Welcome, please register or login.</h1>-->
+        <h1 id="home-message"></h1>
+        <div v-if="!futureRound" id="predictions-banner"></div>
+        <div id="message-and-image"></div>
+        <div v-if="loggedIn && !futureRound" id="predictions"></div>
+            <div v-if="loggedIn && !futureRound" id="currentRoundResults"></div>
+        </div>
+</template>
 
-Vue.use(Vuex);
+<script>
+    import {mapGetters} from "vuex"
 
-export const store = new Vuex.Store({
-    state: {
-        user: localStorage.getItem('user_email') || null,
-        user_first_name: localStorage.getItem('user_first_name') || null,
-    },
-    getters: {
-        loggedIn(state) {
-            return state.user !== null
+    export default {
+        name: 'home',
+        data() {
+            return {
+                futureRound: 0,
+            }
         },
-        userEmail(state) {
-            return state.user
+        computed: {
+            ...mapGetters([
+                'loggedIn'
+            ])
         },
-        userFirstName(state) {
-            return state.user_first_name
-        }
-    },
-    mutations: {
-        loginUser(state, {user_email, user_first_name}) {
-            state.user = user_email;
-            state.user_first_name = user_first_name
+        created() {
+            this.checkFutureRound()
         },
-        logoutUser(state) {
-            state.user = null;
-            state.user_first_name = null
-        }
-    },
-    actions: {
-        emailInDB: function (context, credentials) {
-            return new Promise((resolve, reject) => {
-                axios.post('http://127.0.0.1:5000/check-duplicate-email', {
-                    email: credentials.email
-                })
-                    .then(response => {
-                        return resolve(response.data["result"]);
-                    })
-                    .catch(error => {
-                        console.log(error);
-                        reject(error);
-                    })
-            })
-        },
-
-
-        loginUser(context, credentials) {
-            return new Promise((resolve, reject) => {
-                axios.post('http://127.0.0.1:5000/login-user', {
-                    email: credentials.email,
-                    password: credentials.password,
-                }, {
-                    headers: {
-                        'Content-type': 'application/json',
+        methods: {
+            async checkFutureRound() {
+                const {data} = await this.getFutureRound()
+                const status = data['status'];
+                if (status === 1) {
+                    this.buildFutureRoundMessage(data)
+                    this.futureRound = 1;
+                } else if (status === 0) {
+                    this.futureRound = 0;
+                    if (this.loggedIn) {
+                        this.buildPredictionsMessage()
+                    } else {
+                        this.setupLoggedOut()
                     }
-                })
-                    .then(response => {
-                        const user_email = response.data['user_email'];
-                        const user_first_name = response.data['user_first_name'];
-                        localStorage.setItem('user_email', user_email);
-                        localStorage.setItem('user_first_name', user_first_name);
-                        context.commit('loginUser', {user_email, user_first_name});
-                        resolve(response);
-                    })
-                    .catch(error => {
-                        reject(error);
-                    })
-            })
-        },
-        registerUser(context, credentials) {
-            return new Promise((resolve, reject) => {
-                axios.post('http://127.0.0.1:5000/register-user', {
-                    firstName: credentials.firstName,
-                    lastName: credentials.lastName,
-                    email: credentials.email,
-                    password: credentials.password,
-                })
-                    .then(response => {
-                        resolve(response);
-                    })
-                    .catch(error => {
-                        reject(error);
-                    })
-            })
-        },
-        logoutUser(context) {
-            return new Promise((resolve) => {
-                localStorage.removeItem('user_email');
-                localStorage.removeItem('user_first_name');
-                context.commit('logoutUser');
-                resolve()
-            })
-        },
-        checkFutureRound() {
-            return new Promise((resolve, reject) => {
-                axios.get('http://127.0.0.1:5000/check-future-rounds')
-                .then(response => {
-                    resolve(response);
-                })
-                    .catch(error => {
-                        console.log(error);
-                        reject(error);
-                    })
-                })
+                }
             },
-        //Get user predictions from snail-x-core/core/router.py using email
-        getPredictions() {
-            return new Promise((resolve, reject) => {
-                axios.post('http://127.0.0.1:5000/user-predictions', {
-                    email: localStorage.getItem("user_email")
-                }, {
-                    headers: {
-                        'Content-type': 'application/json',
-                    }
-                })
-                    .then(response => {
-                        resolve(response);
-                    })
-                    .catch(error => {
-                        console.log(error);
-                        reject(error);
-                    })
-            })
-        },
+            getFutureRound() {  // returns 1 if a round exists that has a start datetime later than now, and 0 if no (relies on#
+                                // there never being future rounds if a preceding round is not closed)
+                return this.$store.dispatch('checkFutureRound')
+            },
+            buildFutureRoundMessage(response_data) {
+                var update_text = "Next round opens in ";
+                var days = response_data["days"];
+                if (days === 1) {
+                    update_text = update_text + "1 day, ";
+                } else {
+                    update_text = update_text + days + " days, ";
+                }
+                var hours = response_data["hours"];
+                if (hours === 1) {
+                    update_text = update_text + "1 hour, ";
+                } else {
+                    update_text = update_text + hours + " hours, ";
+                }
+                var minutes = response_data["minutes"];
+                if (minutes === 1) {
+                    update_text = update_text + "and 1 minute";
+                } else {
+                    update_text = update_text + "and " + minutes + " minutes";
+                }
+                document.getElementById("home-message").innerHTML = update_text;
+            },
+            async buildPredictionsMessage() {
+                var response = await this.getPredictions()
+                alert(response.data[0])
+                if (response.data[0] !== "No Open Round") {
+                    if (response.data.message !== "Error. No predictions made") {   // User has made predictions on a currently open round
+                        document.getElementById('predictions-banner').innerHTML = "Your predictions for round " + response.data[0][4] + ":";
+                        var printed_table = '<table><tr><th>Race No.</th><th>Snail No.</th><th>Snail Name</th><th>Trainer</th> </tr>';
 
-        getActiveRound() {
-            return new Promise((resolve, reject) => {
-                axios.get('http://127.0.0.1:5000/get-active-round')
-                    .then(response => {
-                        resolve(response)
+                        for (var y = 0; y < response.data.length; y++) {
+                            printed_table += '<tr><td>' + (y + 1) + '</td><td>' + response.data[y][1] + '</td><td>' + response.data[y][2] + '</td><td>' + response.data[y][3] + '</td></tr>';
+                        }
+                        printed_table += '</table>';
+                    } else if (response.data.message === "Error. No predictions made") {    // User has not made predictions, but a round is open
+                        printed_table = "<center><h3 style='background-color:white; margin-right:30%;'>You have not made any predictions. To do so <a href='snailx.racing'>Click Here</a></h3></center>"
+                        printed_table += "<img height=70% width=70% src=https://static.euronews.com/articles/stories/03/22/91/52/880x495_cmsv2_1f2eea27-fa79-5a58-90f2-c298315d4e68-3229152.jpg>"
+                    }
+                } else {    // There is no open round (ROUND COULD BE INFLIGHT!!!)
+                    alert("boop")
+                    printed_table = "<center><h3 style='background-color:white; padding:5px; margin-right:50%'>No rounds currently open!</h3></center>"
+                }
+                document.getElementById('message-and-image').innerHTML = printed_table;
+
+            },
+            // Returns a response with 'No Open Round' if a round is not open
+            // Returns details of an open round plus predictions if there is an open round
+            getPredictions() {
+                return this.$store.dispatch('getPredictions')
+                    .then((response) => {
+                        return response
                     })
-                    .catch(error => {
-                        alert("errror!")
-                        reject(error);
+            },
+            
+            async setupLoggedOut() {
+                const response = await this.getActiveRound();
+                console.log(response)
+                const round_open = response.data['open']
+                console.log(round_open)
+                var printed_table = "";
+                if (round_open) { // If logged out and there is an open round
+                    printed_table += "<center><h3 style='background-color:white; margin-right:30%;'>A round is now open to predict on - <a href='/#/login'>Log in</a>/<a href='/#/register'>register</a> to play!</h3></center>"
+                }
+                else { // if logged in and there is no future and no open round (INCLUDES INFLIGHT)
+                    const inflight_response = await this.getInflightRound();
+                    if(inflight_response.data['inflight']) {
+                        printed_table += "<center><h3 style='background-color:white; margin-right:30%;'>A round is now in flight - <a href='/#/login'>Log in</a>/<a href='/#/register'>register</a> to view the results!</h3></center>"
+                    } else {
+                        printed_table += "<center><h3 style='background-color:white; margin-right:30%;'>No rounds have been scheduled. <a href='/#/login'>Log in</a>/<a href='/#/register'>register</a> to play!</h3></center>"
+                    }
+                }
+                printed_table += "<img height=70% width=70% src=https://static.euronews.com/articles/stories/03/22/91/52/880x495_cmsv2_1f2eea27-fa79-5a58-90f2-c298315d4e68-3229152.jpg>"
+                document.getElementById('message-and-image').innerHTML = printed_table;
+            },
+
+            getActiveRound() { //  Returns data where ['open'] is True if an open round exists and False if not
+                return this.$store.dispatch('getActiveRound')
+            },
+
+            getInflightRound() {
+                return this.$store.dispatch('getInflightRound')
+                    .then((response) => {
+                        if(response.data[0] !== "No Open Round") {
+                            if (response.data.message !== "Error. No predictions made") {
+                                document.getElementById('predictions-banner').innerHTML = "Your predictions for round " + response.data[0][4] + ":";
+                                var printed_table = '<table><tr><th>Race No.</th><th>Snail No.</th><th>Snail Name</th><th>Trainer</th> </tr>';
+                                for (var y = 0; y < response.data.length; y++) {
+                                    printed_table += '<tr><td>' + (y + 1) + '</td><td>' + response.data[y][1] + '</td><td>' + response.data[y][2] + '</td><td>' + response.data[y][3] + '</td></tr>';
+                                }
+                                printed_table += '</table>';
+                            }
+                            else if (response.data.message == "Error. No predictions made") {
+                                printed_table = "<center><h3 style='background-color:white; margin-right:30%;'>You have not made any predictions. To do so <a href='snailx.racing'>Click Here</a></h3></center>"
+                                printed_table+="<img height=70% width=70% src=https://static.euronews.com/articles/stories/03/22/91/52/880x495_cmsv2_1f2eea27-fa79-5a58-90f2-c298315d4e68-3229152.jpg>"
+                            }
+                        }
+                        else {
+                            printed_table = "<center><h3 style='background-color:white; padding:5px; margin-right:50%'>No rounds currently open!</h3></center>"
+                        }
+                        document.getElementById('predictions').innerHTML = printed_table;
                     })
-            })
+                },
+
+            getCurrentRoundResults() {
+                this.$store.dispatch('getCurrentRoundResults')
+                    .then((response) => {
+                        if (response.data.message !== "Error. No current round results") {
+
+
+                                document.getElementById('predictions-banner').innerHTML = "Your predictions for round " + response.data[0][4] + ":";
+                                var printed_table = '<h3 style="background-color: white">Current Race Results</h3><table><tr><th>Race No.</th><th>Snail Name</th><th>Trainer</th> </tr>';
+
+                                for (var y = 0; y < response.data.length; y++) {
+                                    printed_table += '<tr><td>' + (y + 1) + '</td><td>' + response.data[y][2] + '</td><td>' + response.data[y][3] + '</td></tr>';
+                                }
+                                printed_table += '</table>';
+                            } else {
+                                var printed_table = "<h3 style='background-color: white'>No results avaliable</h3>"
+                            }
+
+                        document.getElementById('currentRoundResults').innerHTML = printed_table;
+                    })
+
+
+            }
         },
-        getCurrentRoundResults() {
-            return new Promise((resolve, reject) => {
-                axios.get('http://127.0.0.1:5000/get-current-round-results')
-                    .then(response => {
-                        resolve(response);
-                    })
-                    .catch(error => {
-                        console.log(error);
-                        reject(error);
-                    })
-            })
-        },
-          
-        getInflightRound() {
-            return new Promise((resolve, reject) => {
-                axios.get('http://127.0.0.1:5000/get-inflight-round')
-                    .then(response => {
-                        resolve(response)
-                    })
-                    .catch(error => {
-                        alert("errror!")
-                        console.log(error);
-                        reject(error);
-                    })
-            })
+        beforeMount() {
+            this.getPredictions()
+            this.getCurrentRoundResults()
         }
     }
-});
+</script>
+
+<style>
+    #home {
+        position: fixed;
+        top: 40%;
+        left: 32%;
+    }
+
+    /*--- future rounds message styling ---*/
+
+    #home-message {
+        background-color: white;
+        /*color: black;*/
+        width: 50%;
+        margin-bottom: 5%;
+        margin-left: 5%;
+        text-align: center;
+    }
+
+    /*--- prediction banner styling ---*/
+
+    #predictions-banner {
+        background-color: white;
+        width: 35%;
+        margin-bottom: 5%;
+        margin-left: 17.5%;
+        text-align: center;
+    }
+
+    /*--- prediction banner styling end ---*/
+
+    /*--- User predictions table styling ---*/
+
+    .home-body {
+        width: 750px;
+        background-color: whitesmoke;
+        border: 1px grey solid;
+        position: fixed;
+        left: 27.5%;
+    }
+    
+    table {
+        width: 70%;
+        background-color: white;
+    }
+
+    table, th, td {
+        border: 1px solid black;
+        border-collapse: collapse;
+    }
+
+    th, td {
+        padding: 15px;
+        text-align: left;
+    }
+
+    /*--- User predictions table styling end ---*/
+</style>
