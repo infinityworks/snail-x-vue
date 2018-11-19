5c5e98221c7b070b9de76476752379d97dacf001
Emily <emily.birch@infinityworks.com>
2018-11-19 12:03:25 +0000

    Fixed register user link - now points to heroku instead of localhost

--- src/store/store.js
+++ src/store/store.js
@@ -80,7 +80,7 @@ export const store = new Vuex.Store({
 
         registerUser(context, credentials) {
             return new Promise((resolve, reject) => {
-                axios.post('https://localhost:5000/register-user', {
+                axios.post('https://snail-x-core.herokuapp.com/register-user', {
                     firstName: credentials.firstName,
                     lastName: credentials.lastName,
                     email: credentials.email,
